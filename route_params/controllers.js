bd819521d3aff5295458b28962fa138377ed48bd
smsohan <sohan39@gmail.com>
2013-04-21 15:36:15 -0600

    route params added

---
+++ route_params/controllers.js
@@ -0,0 +1,14 @@
+var myApp = angular.module('myApp', []);
+
+myApp.config(function($routeProvider){
+
+  $routeProvider.when('/:message', {
+    templateUrl: "app.html",
+    controller: 'AppCtrl'
+  })
+
+});
+
+myApp.controller('AppCtrl', function($scope, $routeParams){
+  $scope.model = {title: "Hello " + ($routeParams.message ? $routeParams.message : 'There')};
+});
