6d2160457730d5a24c4044561f884f8ea342a236
smsohan <sohan39@gmail.com>
2013-04-21 11:13:09 -0600

    netgids added to scope

---
+++ scope_methods/controllers.js
@@ -0,0 +1,21 @@
+//init the app module, no dependencies
+var myApp = angular.module('myApp', []);
+
+//define a factory, aka, service
+myApp.factory('Data', function(){
+  return {message: "I'm shared data from a service"};
+});
+
+// inject the service as a dependency, magically resolved by the name
+function FirstCtrl($scope, Data){
+  $scope.data = Data;
+}
+
+// inject the same service as a dependency, which will share the scope
+function SecondCtrl($scope, Data){
+  $scope.data = Data;
+
+  $scope.reversedMessage = function(message){
+    return message.split("").reverse().join("");
+  };
+}
